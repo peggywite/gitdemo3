cb100a21a592e22fb285e94baf9319e2515b2a82
Kalyanakumar <2415216@cognizant.com>
2026-01-22 09:18:14 +0530

    pushing src folder

---
+++ src/main/java/test/TestClass.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestClass {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, World!123");
+		int a =10;
+		int b=20;
+		int sum = a + b;
+		System.out.println("Sum is: " + sum);
+	}
+}
