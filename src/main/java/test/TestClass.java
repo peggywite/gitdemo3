83a817ea2e8505957a33632d0c8b91160a555b7b
Kalyanakumar <2415216@cognizant.com>
2026-01-22 09:22:17 +0530

    making changes to testclass

--- src/main/java/test/TestClass.java
+++ src/main/java/test/TestClass.java
@@ -4,10 +4,10 @@ public class TestClass {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, World!123");
+		System.out.println("Hello, World!123 from cohort 2");
 		int a =10;
-		int b=20;
-		int sum = a + b;
+		int b=30;
+		int sum = a * b;
 		System.out.println("Sum is: " + sum);
 	}
 }
